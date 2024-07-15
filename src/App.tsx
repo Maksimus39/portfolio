d83d3c51f80fdd7124b68f3d1806d6328345c967
Macsim Minakov <maksim.minakov.83@mail.ru>
2024-07-15 17:44:07 +0300

    Добавление компоненты Main

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css';
 import {Header} from "./layaout/header/Header";
+import {Main} from "./layaout/sections/main/Main";
 
 
 function App() {
     return (
         <div className="App">
             <Header/>
+            <Main/>
         </div>
     );
 }
