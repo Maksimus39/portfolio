73d8ebb1b5b838d267d4db7ba7901b398b088f15
Macsim Minakov <maksim.minakov.83@mail.ru>
2024-07-15 17:44:23 +0300

    Добавление компоненты DownloadCV

---
+++ src/components/downloadCV/DownloadCV.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Icon} from "../icon/Icon";
+
+export const DownloadCV = () => {
+    return (
+        <a href="">
+            <Icon iconID={"DownloadCv2"}/>
+            <Icon iconID={"DownloadCv1"}/>
+            Download CV
+        </a>
+    );
+};
+
