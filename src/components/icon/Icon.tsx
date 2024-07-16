c5dbac0cc39efba1786c9593868cceede80c0863
Macsim Minakov <maksim.minakov.83@mail.ru>
2024-07-16 17:12:41 +0300

    Зарефакторил  компонент Icon

--- src/components/icon/Icon.tsx
+++ src/components/icon/Icon.tsx
@@ -9,7 +9,7 @@ type IconProps = {
 }
 export const Icon = (props: IconProps) => {
     return (
-        <svg width={props.width || "30"} height={props.height || "31"} viewBox={props.viewBox || "0 0 30 31"}
+        <svg width={props.width || "100"} height={props.height || "100"} viewBox={props.viewBox || "0 0 100 100"}
              fill="none" xmlns="http://www.w3.org/2000/svg">
             <use xlinkHref={`${iconsSprite}#${props.iconID}`}/>
         </svg>
