1ad4b1c773a4b01a4fa9ef7360e65c9bf78ecd49
Macsim Minakov <maksim.minakov.83@mail.ru>
2024-07-22 12:41:31 +0300

    Новая компонент Work

---
+++ src/layaout/sections/works/work/Work.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from "styled-components";
+
+export const Work = () => {
+    return (
+        <StyledWork>
+            <Rectangle/>
+        </StyledWork>
+    );
+};
+
+const StyledWork = styled.div``
+const Rectangle = styled.div`
+
+`
