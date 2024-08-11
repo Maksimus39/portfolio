70fd5a27c3b106760275f04d8badfdea5750422e
Macsim Minakov <maksim.minakov.83@mail.ru>
2024-08-11 10:32:39 +0300

    блок Testimony

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import {Header} from "./layaout/header/Header";
 import {Main} from "./layaout/sections/main/Main";
 import {Skills} from "./layaout/sections/skills/Skills";
 import {Works} from "./layaout/sections/works/Works";
+import {Testimony} from "./layaout/sections/testimony/Testymony";
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
             <Main/>
             <Skills/>
             <Works/>
+            <Testimony/>
         </div>
     );
 }
